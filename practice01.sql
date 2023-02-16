e450595cc8554c9ca29f6ead903f4653fac36bb2
Rakshanaa VN <rakshanaa_vn@hotmail.com>
2023-02-16 19:40:16 +0530

    Added my first SQL practice file

---
+++ practice01.sql
@@ -0,0 +1,6 @@
+create table emp (dept_id numeric, emp_id numeric, emp_name varchar(20), salary numeric);
+
+insert into emp values (10, 1001, 'Murali', 10000);
+insert into emp values (20, 2001, 'Raman', 12000);
+
+select * from emp;
